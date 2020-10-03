b782c2f059fe017136922896b814f557e2b2f5dd
Rohit Panjwani <iamrohit.panjwani@gmail.com>
2020-10-03 21:51:08 +0530

    made navbar transparent and fixed

--- src/components/appbar.js
+++ src/components/appbar.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        backgroundColor: 'transparent',
+        height: '7vh',
+        position: 'fixed'
+    }
+}))
 
 function AppBar() {
+
+    const classes = useStyles();
+
     return (
-        <Grid container>
+        <Grid container className={classes.root}>
             Navbar
         </Grid>
     );
