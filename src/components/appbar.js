1af4ba488b243daa4576686d7f5c8a162d7460a9
Rohit Panjwani <iamrohit.panjwani@gmail.com>
2020-10-16 17:37:06 +0530

    navbar ready

--- src/components/appbar.js
+++ src/components/appbar.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Typography, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
+
+import Logo from '../assets/logo.png'
 
 const useStyles = makeStyles(theme => ({
     root: {
         backgroundColor: 'transparent',
-        height: '7vh',
-        position: 'fixed'
+        position: 'fixed',
+    },
+    logo: {
+        height: '50px',
+        width: '50px',
+        margin: '15px'
+    },
+    navItem: {
+        color: 'white',
+        margin: '0 25px'
     }
 }))
 
@@ -15,8 +25,19 @@ function AppBar() {
     const classes = useStyles();
 
     return (
-        <Grid container className={classes.root}>
-            Navbar
+        <Grid container className={classes.root} justify="center" alignItems="center">
+            <Grid item>
+                <Grid container alignItems="center" className={classes.navItem}>
+                    <img src={Logo} className={classes.logo}/>
+                    <Typography variant="h4"> App FrontEnd </Typography>
+                </Grid>
+            </Grid>
+            <Grid item>
+               <Button className={classes.navItem}>New User</Button>
+            </Grid>
+            <Grid item>
+               <Button className={classes.navItem}>Login</Button>
+            </Grid>
         </Grid>
     );
 }
