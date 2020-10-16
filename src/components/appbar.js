8017c232617f1d0f5c6469cdb8e82b8ecb9ae126
Rohit Panjwani <iamrohit.panjwani@gmail.com>
2020-10-16 22:26:17 +0530

    added the register commponent to navbar

--- src/components/appbar.js
+++ src/components/appbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid, Typography, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
+import Register from './register'
 
 import Logo from '../assets/logo.png'
 
@@ -22,22 +23,25 @@ const useStyles = makeStyles(theme => ({
     navItem: {
         margin: '0 25px',
         color: 'inherit'
-    }
+    },
 }))
 
-function AppBar() {
+function Navbar() {
 
     const classes = useStyles();
 
     let listener = null;
     const [scrollState, setScrollState] = React.useState(false);
+    const [dialogs, setDialogs] = React.useState({
+        register: false
+    })
 
     React.useEffect(() => {
         listener = document.addEventListener("scroll", e => {
         var scrolled = document.scrollingElement.scrollTop
         if (scrolled >= 50 && !scrollState) {
             setScrollState(true)
-        } else if (scrolled < 50 && scrollState) {
+        } else if (scrolled < 100 && scrollState) {
             setScrollState(false)
         }
         })
@@ -46,22 +50,40 @@ function AppBar() {
         }
     }, [scrollState])
 
+    function handleCloseRegister() {
+        changeDialogs("register", false);
+    }
+
+    function handleClickNewUser() {
+        changeDialogs("register", true);
+    }
+
+    function changeDialogs(prop, value) {
+        setDialogs(dialogs => ({
+            ...dialogs,
+            [prop]: value
+        }))
+    }
+
     return (
-        <Grid container className={scrollState ? classes.scrolledRoot : classes.root} justify="center" alignItems="center">
-            <Grid item>
-                <Grid container alignItems="center" className={classes.navItem}>
-                    <img src={Logo} className={classes.logo}/>
-                    <Typography variant="h4"> App FrontEnd </Typography>
+        <>
+            <Grid container className={scrollState ? classes.scrolledRoot : classes.root} justify="center" alignItems="center">
+                <Grid item>
+                    <Grid container alignItems="center" className={classes.navItem}>
+                        <img src={Logo} className={classes.logo}/>
+                        <Typography variant="h4"> App FrontEnd </Typography>
+                    </Grid>
+                </Grid>
+                <Grid item>
+                <Button className={classes.navItem} onClick={handleClickNewUser}>New User</Button>
+                </Grid>
+                <Grid item>
+                <Button className={classes.navItem}>Login</Button>
                 </Grid>
             </Grid>
-            <Grid item>
-               <Button className={classes.navItem}>New User</Button>
-            </Grid>
-            <Grid item>
-               <Button className={classes.navItem}>Login</Button>
-            </Grid>
-        </Grid>
+            <Register show={dialogs.register} handleCloseRegister={handleCloseRegister}/>
+        </>
     );
 }
 
-export default AppBar;
+export default Navbar;
