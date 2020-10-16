433b42d6a3161f9d771d708c8dd1dec13bfa72d3
Rohit Panjwani <iamrohit.panjwani@gmail.com>
2020-10-16 18:15:10 +0530

    addes steps to home page

--- src/components/home.js
+++ src/components/home.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Grid, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Intro from '../assets/intro-2.png';
+import Step1 from '../assets/step-1.png';
+import Step2 from '../assets/step-2.png';
+import Step3 from '../assets/step-3.png';
+import Step4 from '../assets/step-4.png';
+import Step5 from '../assets/step-5.png';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
@@ -17,8 +22,9 @@ const useStyles = makeStyles(theme => ({
         color: 'white',
     },
     features: {
-        height: '500px',
-        margin: '0 10%',
+        margin: '0 20%',
+        padding: '2% 0%',
+        marginBottom: '20px',
         boxShadow: '2px 2px 10px 2px gray',
         position: 'relative',
         top: '-100px',
@@ -52,6 +58,13 @@ const useStyles = makeStyles(theme => ({
     socialIcons: {
         cursor: 'pointer',
         margin: '15px'
+    },
+    stepsImg: {
+        height: '450px',
+        width: '600px'
+    },
+    stepDesc: {
+        width: '300px'
     }
 }))
 
@@ -87,6 +100,66 @@ function Home() {
                 </Grid>
             </Grid>
             <Grid item xs={12} className={classes.features}>
+                <Grid container direction="column" alignItems="center" justify="center">
+                    <Grid item>
+                        <h2>How to get started?</h2>
+                    </Grid>
+                    <Grid item>
+                        <Grid container alignItems="center">
+                            <Grid item className={classes.stepDesc}>
+                                <p>Step 1:</p>
+                                <p>Register your business on the site, and create your online presence</p>
+                            </Grid>
+                            <Grid item>
+                                <img src={Step1} alt="" className={classes.stepsImg}></img>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item>
+                        <Grid container alignItems="center">
+                            <Grid item className={classes.stepDesc}>
+                                <p>Step 2:</p>
+                                <p>Share the link with your customers</p>
+                            </Grid>
+                            <Grid item>
+                                <img src={Step2} alt="" className={classes.stepsImg}></img>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item>
+                        <Grid container alignItems="center">
+                            <Grid item className={classes.stepDesc}>
+                                <p>Step 3:</p>
+                                <p>Upload and manage your products on the site</p>
+                            </Grid>
+                            <Grid item>
+                                <img src={Step3} alt="" className={classes.stepsImg}></img>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item>
+                        <Grid container alignItems="center">
+                            <Grid item className={classes.stepDesc}>
+                                <p>Step 4:</p>
+                                <p>Watch your business grow and see analysis of your buyers and products</p>
+                            </Grid>
+                            <Grid item>
+                                <img src={Step4} alt="" className={classes.stepsImg}></img>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item>
+                        <Grid container alignItems="center">
+                            <Grid item className={classes.stepDesc}>
+                                <p>Step 5:</p>
+                                <p>Sit back and relax</p>
+                            </Grid>
+                            <Grid item>
+                                <img src={Step5} alt="" className={classes.stepsImg}></img>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
             </Grid>
         </Grid>
     )
